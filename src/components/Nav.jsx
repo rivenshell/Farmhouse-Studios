8a14fc5e57ba30032dccf35d2e8b0cf4fca7d999
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-06-01 14:14:30 -0400

    navbar update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,19 @@ const Nav = () => {
   return (
     <>
       <div>Nav</div>
+      <ul>
+        <li>
+          <a href="/home" className="site-home">
+            Home
+          </a>
+          <a href="/projects" className="site-projects">
+            Projects
+          </a>
+          <a href="/contact" className="site-contact">
+            Contact
+          </a>
+        </li>
+      </ul>
     </>
   )
 }
