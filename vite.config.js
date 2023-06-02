f1f1692d3a78dc000cef8a5985c7d476b7ea0617
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-06-01 22:20:03 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,4 @@ export default defineConfig({
   root: "src/",
   base: "./",
   plugins: [react()],
-  server: {
-    host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
 })
