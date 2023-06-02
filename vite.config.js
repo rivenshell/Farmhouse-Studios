4f81107f137ef00c4e654aa8644430f7b6b8f999
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-06-01 22:15:55 -0400

    hierchy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src/",
+  base: "./",
   plugins: [react()],
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
 })
