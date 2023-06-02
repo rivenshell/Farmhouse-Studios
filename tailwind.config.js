570836f8400bb16da63650aa9bdb503e6d78261f
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-06-01 22:51:23 -0400

    ver

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["/src/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
